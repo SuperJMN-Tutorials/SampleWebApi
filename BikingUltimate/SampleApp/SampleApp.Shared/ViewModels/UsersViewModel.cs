8f1a8b6d1d54986a19579fab7ac956847e94414d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-03-08 13:15:49 +0100

    Cleanup

--- BikingUltimate/SampleApp/SampleApp.Shared/ViewModels/UsersViewModel.cs
+++ BikingUltimate/SampleApp/SampleApp.Shared/ViewModels/UsersViewModel.cs
@@ -19,15 +19,12 @@ namespace SampleApp.ViewModels
             users = LoadUsers
                 .Select(us =>
                     us.Select(user => new UserViewModel(user)).ToList()).ToProperty(this, model => model.Users);
-            //bikes = this.WhenAnyValue(model => model.SelectedUser)
-            //    .SelectMany(u => bikingService.GetBikes(u.Id))
-            //    .ToProperty(this, model => model.Bikes);
+            
             isBusy = LoadUsers.IsExecuting.ToProperty(this, vm => vm.IsBusy);
         }
 
         public bool IsBusy => isBusy.Value;
 
-        //public ICollection<Bike> Bikes => bikes.Value;
         public ICollection<UserViewModel> Users => users.Value;
 
         public ReactiveCommand<Unit, ICollection<User>> LoadUsers { get; }
